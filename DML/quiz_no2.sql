39ccf7c3eaf9aa12b97ee3f8c2615d921d7392cd
yoodeve <janhold@naver.com>
2023-07-22 12:26:32 +0900

    join 퀴즈 답

--- DML/quiz_no2.sql
+++ DML/quiz_no2.sql
@@ -18,34 +18,34 @@ where
 	
 # 2번
 select
-	ct.category_name,
-	count(ct.category_id * odt.count_number) as total_sold_amount,
-	sum(ct.category_id*pt.product_price) as total_price
+	odt.product_id,
+	count(odt.count_number) as total_sold_amount,
+	sum(pt.product_price * odt.count_number) as total_price
 from 
 	product_tb pt
-	left outer join category_tb ct on(pt.category_id = ct.category_id)
     left outer join order_detail_tb odt on(pt.product_id = odt.product_id)
 group by
-	ct.category_id;
+	odt.product_id;
     
 # 3번  카테고리별 등록된 상품의 count를 조회하시오.
 select
+	pt.category_id,
     ct.category_name,
 	count(pt.product_id) as product_count
 from
 	category_tb ct
     left outer join product_tb pt on(pt.category_id = ct.category_id)
 group by
-	ct.category_id;
+	ct.category_name,
+    pt.category_id; 
 
 # 4번 address_tb sido컬럼을 참조하여 각 지역별로 판매된 상품의 총액을 조회하시오.
 select
-	sum(pt.product_price) as total_sold_price,
-    at.address_sido
+	at.address_sido,
+    ifnull(sum(pt.product_price * odt.count_number), 0) as total_order_price
 from 
 	address_tb at
 	left outer join order_tb ot on(ot.address_id = at.address_id)
-    left outer join user_tb ut on(ut.user_id = ot.user_id)
     left outer join order_detail_tb odt on(odt.order_id = ot.order_id)
     left outer join product_tb pt on (pt.product_id = odt.product_id)
 group by
@@ -53,11 +53,12 @@ group by
 
 # 5번 상품명이 '나이키'가 포함된 제품이 몇개가 판매되었고, 판매된 총액은 얼마인지도 조회하시오.
 select
-	sum(pt.product_price) as nike_price_amount,
-	count(pt.product_name like '%나이키%') as nike_amount
+	'나이키' as search_value,
+    sum(odt.count_number) as nike_sales_amount,
+    sum(pt.product_price * odt.count_number) as total_nike_order_price
 from 
 	order_detail_tb odt
-	left outer join order_tb ot on(ot.order_id = odt.order_id)
     left outer join product_tb pt on(pt.product_id = odt.product_id)
-group by
-	pt.product_name;
+where 
+	pt.product_name like '%나이키%';
+	
